904c67ad62209c6ab6b9c203c54ade3dcfb40748
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-01-29 09:18:04 +0100

    Commit 1
    Program

---
+++ WpfAppBalloon/Balloon.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppBalloon
+{
+    class Balloon
+    {
+        private int x = 10;
+        private int y = 10;
+        private int diameter = 10;
+        private Ellipse ellipse;      
+
+        public Balloon()
+        {
+            CreateEllipse();
+            UpdateEllipse();
+        }
+        
+        public void MoveRight(int xStep)
+        {
+            x = x + xStep;
+            UpdateEllipse();
+        }
+
+        public void MoveUp(int yStep)
+        {
+            y = y - yStep;
+            UpdateEllipse();
+        }
+
+        public void ChangeColor()
+        {
+            Random rndGen = new Random();
+            ellipse.Stroke = new SolidColorBrush(Color.FromRgb((byte) rndGen.Next(0,255), (byte)rndGen.Next(0,255), (byte)rndGen.Next(0,255)));
+            ellipse.Fill = new SolidColorBrush(Color.FromRgb((byte)rndGen.Next(0, 255), (byte)rndGen.Next(0, 255), (byte)rndGen.Next(0, 255)));
+            UpdateEllipse();
+        }
+
+        public void ChangeColor(Brush brush)
+        {
+            ellipse.Stroke = brush;
+            ellipse.Fill = brush;
+        }
+        
+        public void ChangeSize(int change)
+        {
+            diameter = diameter + change;
+            UpdateEllipse();
+        }
+
+        public void DisplayOn(Canvas drawingArea)
+        {
+            drawingArea.Children.Add(ellipse);
+        }
+
+        private void CreateEllipse()
+        {
+            ellipse = new Ellipse();            
+            ellipse.StrokeThickness = 5;
+            ellipse.Stroke = new SolidColorBrush(Colors.BlueViolet);
+        }
+
+        private void UpdateEllipse()
+        {
+            ellipse.Margin = new Thickness(x, y, 0, 0);            
+            ellipse.Height = diameter;
+            ellipse.Width = diameter;
+        }
+        
+    }
+}
--- WpfAppBalloon/MainWindow.xaml.cs
+++ WpfAppBalloon/MainWindow.xaml.cs
@@ -16,12 +16,39 @@ namespace WpfAppBalloon
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// </summary>    
     public partial class MainWindow : Window
     {
+        private Balloon balloon1;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            balloon1 = new Balloon();
+            balloon1.DisplayOn(mijnCanvas);
+
+        }
+
+        private void mijnKnop1_Click(object sender, RoutedEventArgs e)
+        {
+            balloon1.MoveRight(10);
+        }
+
+        private void mijnKnop2_Click(object sender, RoutedEventArgs e)
+        {
+
+            balloon1.ChangeSize(10);
+        }
+
+        private void mijnKnop3_Click(object sender, RoutedEventArgs e)
+        {
+            balloon1.MoveUp(10);
+        }
+
+        private void mijnKnop4_Click(object sender, RoutedEventArgs e)
+        {
+            balloon1.ChangeColor();
         }
     }
 }
